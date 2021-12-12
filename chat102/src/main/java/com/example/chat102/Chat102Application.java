0681996c2ceeebb5b2fb2222178b4ffc399f7b12
laowang <wangxin@itcast.cn>
2021-12-12 18:19:36 +0800

    update 启动类，添加exclude配置解决datasource 启动报错 update pom.xml，添加mybatis plus以及jdbc配置

--- chat102/src/main/java/com/example/chat102/Chat102Application.java
+++ chat102/src/main/java/com/example/chat102/Chat102Application.java
@@ -2,8 +2,9 @@ package com.example.chat102;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Chat102Application {
 
     public static void main(String[] args) {
