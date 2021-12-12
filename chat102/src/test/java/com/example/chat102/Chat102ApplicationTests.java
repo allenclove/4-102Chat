39b68cfd0ce3e2977a2e1e48b79e32037f01e2f2
白狐 <1412629317@qq.com>
2021-12-12 15:32:38 +0800

    初始化项目

---
+++ chat102/src/test/java/com/example/chat102/Chat102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chat102ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
